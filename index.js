aff50bfaf4f41c1927077f403917c1bd838de30c
tamybl <tamara.bravo.l@gmail.com>
2018-02-19 00:30:17 -0300

    Cambio configuracion de puerto

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ io.on('connection', function (socket) {
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -92,7 +93,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
 
